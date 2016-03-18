88cc5b4f9f805aa35cd5a7b44f0431f5019576a2
Adam Heurlin <adamheurlin@gmail.com>
2016-03-18 21:10:49 +0100

    Use 'use x' instead of 'use namespace x'

--- build.bat
+++ build.bat
@@ -6,10 +6,10 @@ rem Path to the library folder
 set LIB=%OSP%\lib
 
 echo [!] Compiling osprey.interpreter...
-%OSPC% /libpath "%LIB%" /verbose /type module /out "%LIB%\osprey.interpreter\osprey.interpreter.ovm" /name osprey.interpreter /doc "%LIB%\osprey.interpreter\osprey.interpreter.ovm.json" /formatjson osprey.interpreter\osprey.interpreter.osp
+%OSPC% /libpath "%LIB%" /import osprey.compiler /verbose /type module /out "%LIB%\osprey.interpreter\osprey.interpreter.ovm" /name osprey.interpreter /doc "%LIB%\osprey.interpreter\osprey.interpreter.ovm.json" /formatjson osprey.interpreter\osprey.interpreter.osp
 
 if %ERRORLEVEL%==0 (
 	echo.
 	echo [!] Compiling ospi...
-	%OSPC% /libpath "%LIB%" /verbose /main osprey.interpreter.main /out bin\ospi.ovm /name ospi /doc bin\ospi.ovm.json /formatjson ospi\ospi.osp
+	%OSPC% /libpath "%LIB%" /import osprey.compiler /import osprey.interpreter /verbose /main osprey.interpreter.main /out bin\ospi.ovm /name ospi /doc bin\ospi.ovm.json /formatjson ospi\ospi.osp
 )
